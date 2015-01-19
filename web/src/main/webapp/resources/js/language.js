f7bfb3ac39df8897170ae0e673c95d77c4af87b4
p.rodimova <P.Rodimova@qiwi.ru>
2015-01-20 00:18:29 +0300

    language selector works!

--- web/src/main/webapp/resources/js/language.js
+++ web/src/main/webapp/resources/js/language.js
@@ -1,19 +1,33 @@
 function language() {
     var languageSelect = document.getElementById('selectedLanguage').value;
     $.post('language/set', {language: languageSelect });
+    location.reload();
 
 }
 
 languageSelectorLoaded = false;
 function detectLanguage() {
     if (!languageSelectorLoaded) {
-        var userLang = navigator.language || navigator.userLanguage;
-        var boxValue = 'en';
-        if (userLang == 'en' || userLang == 'ru') {
-            boxValue = userLang;
-        }
-        $("select#selectedLanguage").val(boxValue).value = 'true';
-        $.post('language/set', {language: userLang});
-        languageSelectorLoaded = true;
+        var boxValue = null;
+        $.post('language/get', function (result) {
+            boxValue = result;
+            if (boxValue != 'en' && boxValue != 'ru') {
+                //first load
+                boxValue = 'en';
+                var userLang = navigator.language || navigator.userLanguage;
+                if (userLang != null && userLang.match(/.*en.*/)) {
+                    boxValue = 'en';
+                } else if (userLang != null && userLang.match(/.*ru.*/)) {
+                    boxValue = 'ru'
+                }
+                $.post('language/set', {language: boxValue});
+                if (boxValue =='ru') {
+                    //по умолчанию английский, поэтому перезагружаем
+                    location.reload();
+                }
+            }
+            $("select#selectedLanguage").val(boxValue);
+            languageSelectorLoaded = true;
+        });
     }
 }
