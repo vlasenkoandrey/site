f0616065805cb7821bab93672befe85e739138d8
Andrey Vlasenko <andrvlas@gmail.com>
2017-05-30 16:49:25 +0300

    more

--- docker/step2/entrypoint.sh
+++ docker/step2/entrypoint.sh
@@ -4,5 +4,6 @@ cd /opt/projects/vlasenkosite/web/build/distributions
 
 java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=4000,suspend=n  -cp "./*" cc.vlasenko.site.MainApplication
 
+service nginx restart
 nginx -s reload
 
