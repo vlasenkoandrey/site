4672d4ab7f29173cb5a47e291a9b45810bea9fcf
Andrey Vlasenko <andrvlas@gmail.com>
2017-06-14 19:42:13 +0300

    lang

--- web/src/main/webapp/WEB-INF/jsp/layout.jsp
+++ web/src/main/webapp/WEB-INF/jsp/layout.jsp
@@ -36,8 +36,9 @@
     <script src="../resources/js/language.js"></script>
     <script src="../resources/js/layout.js"></script>
     <script type="text/javascript">
+    detectLanguage();
         $(document).ready(function() {
-//            detectLanguage();
+            loadLanguage();
             $('#fullpage').fullpage({
                 anchors: ['pageWeAre', 'pageTheStory', 'pageTravelling', 'pageGallery', 'pageJob'],
                 verticalCentered: true,
@@ -146,12 +147,12 @@
 <div class="share_button googleplus" onclick="shareGoog('http://vlasenko.cc/')"></div>
 <div class="share_button twitter" onclick="tweet('familyvlasenko')"></div>
 <div class="share_button vk" onclick="shareVk('http://vlasenko.cc/', 'http://vlasenko.cc/static/img/share/weAreShare.png')"></div>
-<%--<div class="language" onchange="setLanguage()">--%>
-    <%--<select id="selectedLanguage">--%>
-        <%--<option id="english" class="langEn" value="en">English</option>--%>
-        <%--<option id="russian" class="langRu" value="ru">Russian</option>--%>
-    <%--</select>--%>
-<%--</div>--%>
+<div class="language" onchange="setLanguage()">
+    <select id="selectedLanguage">
+        <option id="english" class="langEn" value="en">English</option>
+        <option id="russian" class="langRu" value="ru">Russian</option>
+    </select>
+</div>
 <script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
--- web/src/main/webapp/resources/js/language.js
+++ web/src/main/webapp/resources/js/language.js
@@ -2,32 +2,32 @@ function setLanguage() {
     var languageSelect = document.getElementById('selectedLanguage').value;
     $.post('language/set', {vlanguage: languageSelect });
     location.reload();
+}
+
+function detectLanguage() {
+    var boxValue = null;
+    $.post('language/get', function (result) {
+                boxValue = result;
+                if (boxValue != 'en' && boxValue != 'ru') {
+                    //first load
+                    boxValue = 'en';
+                    var userLang = navigator.language || navigator.userLanguage;
+                    if (userLang != null && userLang.match(/.*en.*/)) {
+                        boxValue = 'en';
+                    } else if (userLang != null && userLang.match(/.*ru.*/)) {
+                        boxValue = 'ru'
+                    }
+                    $.post('language/set', {vlanguage: boxValue});
+     }
+
+     return boxValue;
 
 }
 
-languageSelectorLoaded = false;
-function detectLanguage() {
-    if (!languageSelectorLoaded) {
-        var boxValue = null;
-        $.post('language/get', function (result) {
-            boxValue = result;
-            if (boxValue != 'en' && boxValue != 'ru') {
-                //first load
-                boxValue = 'en';
-                var userLang = navigator.language || navigator.userLanguage;
-                if (userLang != null && userLang.match(/.*en.*/)) {
-                    boxValue = 'en';
-                } else if (userLang != null && userLang.match(/.*ru.*/)) {
-                    boxValue = 'ru'
-                }
-                $.post('language/set', {vlanguage: boxValue});
-                if (boxValue =='ru') {
-                    //по умолчанию английский, поэтому перезагружаем
-//                    location.reload();
-                }
-            }
+function loadLanguage() {
+        var boxValue = detectLanguage();
+        if(boxValue) {
             $("select#selectedLanguage").val(boxValue);
-            languageSelectorLoaded = true;
-        });
-    }
+        }
+        location.reload();
 }
