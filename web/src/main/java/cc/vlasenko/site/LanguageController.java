03e505f4289d488f4f2fe75e0e23703d33239839
Andrey Vlasenko <andrvlas@gmail.com>
2017-06-14 20:03:56 +0300

    lang

--- web/src/main/java/cc/vlasenko/site/LanguageController.java
+++ web/src/main/java/cc/vlasenko/site/LanguageController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class LanguageController extends LocalisingController {
 
-	@RequestMapping("/language/set", method = RequestMethod.POST)
+	@RequestMapping(value = "/language/set", method = RequestMethod.POST)
 	@ResponseStatus(value = HttpStatus.OK)
 	public void setLanguage(ModelMap modelMap, @RequestParam("vlanguage") String vlanguage) {
 		if (!StringUtils.isEmpty(vlanguage)) {
